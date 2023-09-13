007df9b09a114b5dd854329751db42b002bf18a1
searchsakib <searchsakib@gmail.com>
2023-09-13 18:06:29 +0600

    1 card with mock data, done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,10 +6,19 @@ const Card = () => {
           <img src="https://i.ibb.co/FK52dyd/prod6.png" alt="car!" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Life hack</h2>
-          <p>How to park your car at your garage?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Learn now!</button>
+          <div className="flex  items-center ">
+            <h2 className="card-title text-warning">Modern Sofa</h2>
+            <p className="text-right font-semibold text-warning">
+              Price: <span>799.99</span>${' '}
+            </p>
+          </div>
+
+          <p className="text-center text-white">
+            A sleek and comfortable modern sofa for your living room.
+          </p>
+          <p className="text-center   ">Category: Living Room</p>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary mt-4">Buy now</button>
           </div>
         </div>
       </div>

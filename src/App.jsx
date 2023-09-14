99cfde7f86c10f9bc97d0e7c12864edd28ed02d9
searchsakib <searchsakib@gmail.com>
2023-09-14 12:26:21 +0600

    some small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import Card from './components/Card/Card';
+import Carts from './components/Carts/carts';
 import Nav from './components/Nav/Nav';
 
 function App() {
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div>
       <Nav></Nav>
-      <Card></Card>
+      <div className="max-w-screen-xl mx-auto flex gap-10">
+        <div className="w-2/3">
+          <Card></Card>
+        </div>
+        <div className="w-1/3">
+          <Carts></Carts>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Carts/Carts.jsx
@@ -0,0 +1,9 @@
+const Carts = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Carts;
